ea5afc125d77ff38ac9280897df0eb8dfa73d846
Phước Sang <thps1411@gmail.com>
2026-07-16 19:05:20 +0700

    Implement MonHoc class with properties and methods
    Added fields and methods for course management.

--- src/model/MonHoc.java
+++ src/model/MonHoc.java
@@ -1,4 +1,32 @@
 package model;
 
 public class MonHoc {
+    private String maMon;
+    private String tenMon;
+
+    public MonHoc(String maMon, String tenMon) {
+        this.maMon = maMon;
+        this.tenMon = tenMon;
+    }
+
+    public String getMaMon() {
+        return maMon;
+    }
+
+    public void setMaMon(String maMon) {
+        this.maMon = maMon;
+    }
+
+    public String getTenMon() {
+        return tenMon;
+    }
+
+    public void setTenMon(String tenMon) {
+        this.tenMon = tenMon;
+    }
+
+    @Override
+    public String toString() {
+        return maMon + " - " + tenMon;
+    }
 }
