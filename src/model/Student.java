5cf847512a82972f931b6e78593d9d32437751e7
Phước Sang <thps1411@gmail.com>
2026-07-16 16:20:26 +0700

    Implement Student class with attributes and methods
    Added properties and methods for Student class.

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,50 @@
 package model;
 
-public class Student {
+public class Student extends Person{
+    private String lop;
+    private String monHoc;
+    private double diem;
+
+    public Student(String id, String name, String lop, String monHoc, double diem) {
+        super(id, name);
+        this.lop = lop;
+        this.monHoc = monHoc;
+        this.diem = diem;
+    }
+
+    public String getLop() {
+        return lop;
+    }
+
+    public void setLop(String lop) {
+        this.lop = lop;
+    }
+
+    public String getMonHoc() {
+        return monHoc;
+    }
+
+    public void setMonHoc(String monHoc) {
+        this.monHoc = monHoc;
+    }
+
+    public double getDiem() {
+        return diem;
+    }
+
+    public void setDiem(double diem) {
+        this.diem = diem;
+    }
+    
+    //ghi de phuong thuc
+    @Override
+    public void display() {
+        System.out.printf("%-10s %-20s %-10s %-15s %-5.2f\n", getId(), getName(), lop, monHoc, diem);
+    }
+
+    //chuyen doi tuong thanh chuoi de luu file
+    @Override
+    public String toString() {
+        return getId() + ";" + getName() + ";" + lop + ";" + monHoc + ";" + diem;
+    }
 }
