07a77c7c081ac75849b086917e47e8cc0fb2536d
Phước Sang <thps1411@gmail.com>
2026-07-16 19:04:53 +0700

    Implement LopHoc class with properties and methods
    Added fields and methods for class LopHoc.

--- src/model/LopHoc.java
+++ src/model/LopHoc.java
@@ -1,4 +1,32 @@
 package model;
 
 public class LopHoc {
+    private String maLop;
+    private String tenLop;
+
+    public LopHoc(String maLop, String tenLop) {
+        this.maLop = maLop;
+        this.tenLop = tenLop;
+    }
+
+    public String getMaLop() {
+        return maLop;
+    }
+
+    public void setMaLop(String maLop) {
+        this.maLop = maLop;
+    }
+
+    public String getTenLop() {
+        return tenLop;
+    }
+
+    public void setTenLop(String tenLop) {
+        this.tenLop = tenLop;
+    }
+
+    @Override
+    public String toString() {
+        return maLop + " - " + tenLop;
+    }
 }
