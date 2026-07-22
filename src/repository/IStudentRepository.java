80366021865cad573636b04f41f68216aaa13f56
Phước Sang <thps1411@gmail.com>
2026-07-22 21:11:01 +0700

    Add method to update student in repository interface

--- src/repository/IStudentRepository.java
+++ src/repository/IStudentRepository.java
@@ -7,6 +7,7 @@ public interface IStudentRepository {
     List<Student> layDanhSachSinhVien();
     void themSinhVien(Student sinhVien);
     void xoaSinhVien(String id);
+    void capNhatSinhVien(Student sinhVien);
     void ghiFile();
     void docFile();
 }
